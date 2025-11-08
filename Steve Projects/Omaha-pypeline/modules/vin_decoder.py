f4c65280cf4e4260df94e8775dc06cee16c0eb90
Stephen Watts <smwatts@mail.mccneb.edu>
2025-11-07 20:37:47 -0600

    fixed hopefully a diff module though

--- Steve Projects/Omaha-pypeline/modules/vin_decoder.py
+++ Steve Projects/Omaha-pypeline/modules/vin_decoder.py
@@ -1,8 +1,20 @@
-import requests
+def flatten_value(val):
+
+    if isinstance(val, str):
+        # If more than half of the chars are newlines, flatten it
+        if val.count('\n') > len(val) // 4:
+            # Remove newlines and spaces, then return as one word
+            return val.replace('\n', '').replace(' ', '')
+        # Otherwise, just replace newlines with spaces for safety
+        return val.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ')
+    return val
 
 def decode_vin(vin):
     url = f"https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/{vin}?format=json"
     response = requests.get(url)
     data = response.json()
-    result = {item['Variable']: item['Value'] for item in data['Results'] if item['Value']}
+    result = {
+        item['Variable']: flatten_value(item['Value'])
+        for item in data['Results'] if item['Value']
+    }
     return result
